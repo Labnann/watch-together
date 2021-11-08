8e1319df4eee53a1a5572587d05991c3ad8afbc9
Labnan <khalidmasum@iut-dhaka.edu>
2021-11-08 12:29:54 +0600

    Implement Play and Pause

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,8 @@
   const video = document.getElementById("shared_video");
   const videoSourceSendButton = document.getElementById("set_video_source");
   const videoSourceText = document.getElementById("video_source_text");
-
+  
+  let isPlaying = false;
   let isLocked = false;
 
   const sendObject = (object) => {
@@ -21,15 +22,24 @@
       isLocked = false;
     }
 
-    setTimeout(unlock,250);
+    setTimeout(unlock,100);
 
+  }
+
+  function changePlayStatus(){
+    isPlaying = !isPlaying;
+    if(!isPlaying) video.pause();
+    else video.play();
   }
 
   socket.on("object", (object) => {
     lockSocket();
     video.currentTime = object.currentTime;
+    if(object.changePlayingStatus) changePlayStatus();
     unlockSocket();
   })
+
+
 
 
   videoSourceSendButton.onclick = () => {
@@ -41,6 +51,24 @@
   video.onseeked = (evet) => {
     sendObject({currentTime: video.currentTime})
   }
+  
+  video.onplay = ()=>{
+    isPlaying = true;
+    sendObject({
+      currentTime: video.currentTime,
+      changePlayingStatus: true
+    })
+  }
+
+  video.onpause = () =>{
+   isPlaying = false;
+    sendObject({
+      currentTime: video.currentTime,
+      changePlayingStatus: true
+    })
+ 
+  }
+  
 
   
 
